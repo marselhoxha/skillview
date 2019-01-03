5e1c6173ae0093c3e30a4c8a0c9521b8366f9f80
ivan <ivan.trabucco@yahoo.it>
2019-01-03 16:44:45 +0100

    creato repo figura professionale

---
+++ SkillView/SkillViewBE/src/main/java/repository/FigProfessionaleImp.java
@@ -0,0 +1,39 @@
+package repository;
+
+import java.util.ArrayList;
+
+import model.FiguraProfessionale;
+
+public class FigProfessionaleImp implements FigProfessionaleRep {
+
+	@Override
+	public boolean insertFigProf(FiguraProfessionale obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<FiguraProfessionale> findAllFigProf(FiguraProfessionale obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FiguraProfessionale findForIdFigProf(FiguraProfessionale obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateFigProf(FiguraProfessionale obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteFigProf(FiguraProfessionale obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
